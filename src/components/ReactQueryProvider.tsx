b56bc220331babe9d8e50055361c901ccdd9e65f
Sebastian Loguzzo <sebastian.loguzzo@gmail.com>
2025-05-24 08:43:38 -0300

    add Firebase integration and public wishes functionality with React Query

---
+++ src/components/ReactQueryProvider.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { ReactNode } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
+export const ReactQueryProvider = ({ children }: { children: ReactNode }) => {
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    );
+};
